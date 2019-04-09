e3beead73f9e6cf8dc59962c73ed35894dc8d2ab
Damian Koch <Damian Koch@localhost>
2019-04-09 11:11:32 +0200

    working on throw

--- Assets/Scripts/Airstream.cs
+++ Assets/Scripts/Airstream.cs
@@ -7,6 +7,7 @@ public class Airstream : MonoBehaviour
     private Vector3 direction;
     [SerializeField]
     private float strength;
+    List<Collider> colliders = new List<Collider>();
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,10 @@ public class Airstream : MonoBehaviour
     {
         if (other.GetComponent<AirstreamAffected>() != null)
         {
+            if (colliders.Contains(other) == false)
+            {
+                colliders.Add(other);
+            }
             AirstreamAffected airstreamAffected = other.GetComponent<AirstreamAffected>();
             airstreamAffected.airstreamMotion = direction * strength;
             airstreamAffected.inAirstream = true;
@@ -31,8 +36,10 @@ public class Airstream : MonoBehaviour
     {
         if (other.GetComponent<AirstreamAffected>() != null)
         {
-            AirstreamAffected airstreamAffected = other.GetComponent<AirstreamAffected>();
-            airstreamAffected.inAirstream = false;
+            if (colliders.Contains(other)){
+                colliders.Remove(other);
+            }
+            other.GetComponent<AirstreamAffected>().inAirstream = false;
         }
     }
 
@@ -42,6 +49,13 @@ public class Airstream : MonoBehaviour
     {
         MeshRenderer mr = GetComponent<MeshRenderer>();
         BoxCollider bc = GetComponent<BoxCollider>();
+        if (mr.enabled)
+        {
+            foreach (Collider collider in colliders)
+            {
+                collider.GetComponent<AirstreamAffected>().inAirstream = false;
+            }
+        }
         mr.enabled = !mr.enabled;
         bc.enabled = !bc.enabled;
     }
