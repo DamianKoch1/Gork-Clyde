4bb96bfa914cd851f56612a23a90584d3ffaaa7f
Damian <Damian@localhost>
2019-04-25 15:11:05 +0200

    added main menu

---
+++ Assets/UI/Buttons/MenuButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class MenuButton : MonoBehaviour
+{
+   
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
