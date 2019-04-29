64957971c6849c64422d4f16f6fa5028d5840672
Damian <Damian@localhost>
2019-04-29 13:17:59 +0200

    (unstable) working on clide sliding off boxes

---
+++ Assets/Players/AttachObjectOnTop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachObjectOnTop : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger == false)
+        {
+            other.transform.SetParent(target, true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.parent == target)
+        {
+            other.transform.SetParent(null, true);
+        }
+    }
+}
