aa85a26b91f7f2d3942faa6018b9db01202083d1
Damian <Damian@localhost>
2019-05-03 17:23:24 +0200

    fixed objects bouncing at top of platform

--- Assets/Interactables/Platforms/MovingPlatform.cs
+++ Assets/Interactables/Platforms/MovingPlatform.cs
@@ -5,33 +5,24 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     [SerializeField]
-    private GameObject start, target;
+    private GameObject start, target, platform;
     private Vector3 pos1, pos2;
     private bool stop;
-    private Vector3 startPos;
-    private Vector3 targetPos;
-    public enum Mode {Autostart, Continuous, Oneshot, PressurePlate};
+    public enum Mode {Autostart, Continuous, Oneshot};
     public Mode mode = Mode.Autostart;
     [SerializeField]
     private float speed;
     private Rigidbody rb;
-   
+    private float moveAmount = 0;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = platform.GetComponent<Rigidbody>();
         pos1 = start.transform.position;
-        startPos = transform.position;
         pos2 = target.transform.position;
-        targetPos = pos1;
         if (mode == Mode.Autostart)
         {
-            Move();
-        }
-        else if (mode == Mode.PressurePlate)
-        {
-            Move();
-            stop = true;
+            stop = false;
         }
         else
         {
@@ -42,45 +33,27 @@ public class MovingPlatform : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        moveAmount += Time.fixedDeltaTime * speed;
+        if (moveAmount >= Mathf.PI * 20) moveAmount = 0;
+        
         if (stop == false)
         {
-            rb.MovePosition(rb.position + (targetPos - startPos).normalized/10 * speed);
-            if (Vector3.Distance(rb.position, targetPos) < 0.1f)
+            rb.MovePosition(pos1 + 0.5f*(1+Mathf.Sin(moveAmount))*(pos2 - pos1));
+            if ((Vector3.Distance(rb.position, pos1) < 0.1f) || (Vector3.Distance(rb.position, pos2) < 0.1f))
             {
-               if (mode != Mode.Oneshot)
-               {
-                    Move();
-               }
-               else
+               if (mode == Mode.Oneshot)
                {
                     stop = true;
                }
             }
         }
-
-
     }
 
-    void Move()
-    {
-        stop = false;
-        if (targetPos == pos1)
-        {
-            targetPos = pos2;
-        }
-        else
-        {
-            targetPos = pos1;
-        }
-        startPos = transform.position;
-    }
-
-   
     public void OnButtonActivated()
     {
-        if (Vector3.Distance(rb.position, targetPos) < 0.1f)
+        if (Mathf.Abs(Mathf.Sin(moveAmount)) < 0.45f)
         {
-            Move();
+            stop = false;
         }
     }
     
