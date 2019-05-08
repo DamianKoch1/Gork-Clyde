52616798fecb7a2b2a533d25266792c697254fb0
Damian <Damian@localhost>
2019-05-08 17:07:28 +0200

    set up global sounds, added button click sound

---
+++ Assets/Audio/KeepAudioLoaded.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepAudioLoaded : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameObject.FindGameObjectsWithTag("bgm").Length > 1 && CompareTag("bgm"))
+        {
+            Destroy(gameObject);
+        }
+        if (GameObject.FindGameObjectsWithTag("sfx").Length > 1 && CompareTag("sfx"))
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
