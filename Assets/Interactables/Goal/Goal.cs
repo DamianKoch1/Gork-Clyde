e153daac7fa3f3143952e317249062ccb886d685
Damian <Damian@localhost>
2019-05-09 13:08:12 +0200

    added:         -goal         -inputs when character selection is skipped (playtesting)         -goal placeholder changed:         -renamed clide to clyde in most files         -camera focuses one player when other cant move

---
+++ Assets/Interactables/Goal/Goal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+
+   private int playerCount = 0;
+   [SerializeField] 
+   private string nextLevelName;
+   
+   private void OnTriggerEnter(Collider other)
+   {
+      if (!other.isTrigger && other.GetComponent<Player>())
+      {
+         other.GetComponent<Player>().canMove = false;
+         if (other.GetComponent<Player>().anim)
+         {
+            other.GetComponent<Player>().anim.enabled = false;
+         }
+         if (playerCount == 1)
+         {
+            LoadNextLevel();
+         }
+         else
+         {
+            playerCount++;
+         }
+      }
+   }
+
+   void LoadNextLevel()
+   {
+      SceneManager.LoadScene(nextLevelName);
+   }
+   
+}
+
--- Assets/Players/Clyde.cs
+++ Assets/Players/Clyde.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Clyde : Player
 {
-    public static string _xAxis, _zAxis, _jumpButton;
+    public static string _xAxis = "ClydeHorizontal", _zAxis = "ClydeVertical", _jumpButton = "ClydeJump";
 
     private void Start()
     {
         base.Start();
+        //for playtesting (if somehow character selection is skipped)
+        if (String.IsNullOrEmpty(_xAxis))
+        {
+            _xAxis = "ClydeHorizontal";
+            _zAxis = "ClydeVertical";
+            _jumpButton = "ClydeJump";
+        }
+        //        
         InitializeInputs(_xAxis, _zAxis, _jumpButton);
     }
 }
