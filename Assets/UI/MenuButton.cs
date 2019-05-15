22b50558306970bd43d4af59e36518b2375e7c6f
Damian <Damian@localhost>
2019-05-15 15:34:34 +0200

    restructured ui folder, button functions are in menu script, button script only plays click sound

---
+++ Assets/UI/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip clickSound;
+    
+    
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(PlayClickSound);
+    }
+
+    private void PlayClickSound()
+    {
+        GameObject.FindGameObjectWithTag("sfx").GetComponent<AudioSource>().clip = clickSound;
+        GameObject.FindGameObjectWithTag("sfx").GetComponent<AudioSource>().Play();
+    }
+}
