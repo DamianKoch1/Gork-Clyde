8923ea8aeb223ed6e38c32a88f16f9e9b933c8de
Damian <Damian@localhost>
2019-05-20 12:34:04 +0200

    added spawnpoint / deathzone

---
+++ Assets/Interactables/Spawnpoint/Spawnpoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnpoint : MonoBehaviour
+{
+    [SerializeField]
+    public static GameObject SPAWNPOINT;
+
+    private void Start()
+    {
+        SPAWNPOINT = gameObject;
+    }
+}
