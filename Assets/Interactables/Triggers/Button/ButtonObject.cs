fb73fe71cc1b268c250d10b3032619d85c048fa2
Damian <Damian@localhost>
2019-05-29 12:43:12 +0200

    used inheritance for trigger objects

--- Assets/Interactables/Triggers/Button/ButtonObject.cs
+++ Assets/Interactables/Triggers/Button/ButtonObject.cs
@@ -2,58 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonObject : MonoBehaviour
+public class ButtonObject : TriggerObject
 {
-    [SerializeField]
-    private GameObject[] targets;
+    
     [SerializeField]
     private bool oneTimeUse;
     private bool triggered;
-    private enum TriggerableBy {Gork, Clide, All};
-    [SerializeField]
-    private TriggerableBy triggerableBy = TriggerableBy.All;
     
-    private void OnTriggerEnter(Collider other)
-    {
-        if (!other.isTrigger)
-        {
-            if (triggerableBy == TriggerableBy.All)
-            {
-                if (other.CompareTag("player") || other.CompareTag("pushable"))
-                {
-                    SendTriggered();
-                }
-            } else if (triggerableBy == TriggerableBy.Gork && other.GetComponent<Gork>())
-            {
-                SendTriggered();
-            } else if (triggerableBy == TriggerableBy.Clide && other.GetComponent<Clyde>())
-            {
-                SendTriggered();
-            }
-        }
-    }
-
-
-    void SendTriggered()
+  
+    protected override void OnTriggered()
     {
         if (oneTimeUse)
         {
-            if (!triggered)
-            {
-                GetComponent<AudioSource>().Play();
-                triggered = true;
-                foreach(GameObject target in targets) {
-                    target.SendMessage("OnButtonActivated");
-                }
-            }
+            if (triggered) return;
+            triggered = true;
         }
-        else
+        GetComponent<AudioSource>().Play();
+        foreach (GameObject target in targets)
         {
-            GetComponent<AudioSource>().Play();
-            foreach (GameObject target in targets)
-            {
-                target.SendMessage("OnButtonActivated");
-            }
+            target.SendMessage("OnButtonActivated");
         }
     }
 }
