e38658e7cb0641baac760b22eb45a5ee49a2840f
Damian <Damian@localhost>
2019-06-18 19:14:27 +0200

    Added loading screen (progress jumps up if loading small scenes)

---
+++ Assets/UI/LoadingScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LoadingScreen : MonoBehaviour
+{
+    public static string NEXT_LEVEL_NAME;
+
+    [SerializeField]
+    private Image loadingBar;
+
+    private void Start()
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    private IEnumerator LoadNextLevel()
+    {
+        AsyncOperation loading = SceneManager.LoadSceneAsync(NEXT_LEVEL_NAME);
+        loadingBar.fillAmount = 0;
+        while (!loading.isDone)
+        {
+            loadingBar.fillAmount = loading.progress;
+            yield return null;
+        }
+    }
+}
