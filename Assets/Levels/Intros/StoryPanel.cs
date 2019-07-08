1ceb99dc1030765963afab69d6a3ffc3464ddd79
Damian <Damian@localhost>
2019-07-08 12:54:09 +0200

    loading screen shows after story panels, just before level loads

---
+++ Assets/Levels/Intros/StoryPanel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryPanel : MonoBehaviour
+{
+	public void LoadNextPanel(string name)
+	{
+		SceneManager.LoadScene(name);
+	}
+
+	public void LoadLevel(string name)
+	{
+		LoadingScreen.NEXT_LEVEL_NAME = name;
+		SceneManager.LoadScene("Loading Screen");
+	}
+}
