4eafbddf497d6eed1efb7ad219f61d8f4cab1dcd
Damian <Damian@localhost>
2019-07-15 12:07:51 +0200

    changed: -can push diagonally -shrinked push trigger collider -> can only push when directly facing pushable side midle
    fixed:
    -being able to push big pushable by walking
    
    in progress:
    -setting position/rotation when starting to push

--- Assets/Interactables/Pushables/Big/PushableBig.cs
+++ Assets/Interactables/Pushables/Big/PushableBig.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PushableBig : Pushable
 {
 
+    /// <summary>
+    /// Enables gork to push this object while in trigger
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerStay(Collider other)
     {
         if (other.isTrigger) return;
@@ -14,13 +18,17 @@ public class PushableBig : Pushable
         gork.pushedObj = gameObject;
     }
 
+    /// <summary>
+    /// Prevents gork from pushing this object after leaving it
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerExit(Collider other)
     {
         if (other.isTrigger) return;
         var gork = other.GetComponent<Gork>();
         if (!gork) return;
-        if (!gork.pushedObj) return;
-
+        if (gork.pushing) return;
+        
         gork.pushedObj = null;
     }
 
