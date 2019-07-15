aa83e971d0084fa3a05c62f677a7e848c9ad7279
Damian <Damian@localhost>
2019-07-15 12:48:56 +0200

    gork moves to pushable side middle when pushing

--- Assets/Interactables/Pushables/Big/PushableBig.cs
+++ Assets/Interactables/Pushables/Big/PushableBig.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PushableBig : Pushable
 {
 
+    [SerializeField]
+    private float gorkPushDistance = 0.7f;
+    
     /// <summary>
     /// Enables gork to push this object while in trigger
     /// </summary>
@@ -32,4 +35,28 @@ public class PushableBig : Pushable
         gork.pushedObj = null;
     }
 
+    public Vector3 GetClosestPushPosition(Vector3 objPosition)
+    {
+        Vector3[] gorkPushPositions = new Vector3[6];
+        
+        gorkPushPositions[0] = transform.position + transform.up * gorkPushDistance;
+        gorkPushPositions[1] = transform.position - transform.up * gorkPushDistance;
+        gorkPushPositions[2] = transform.position + transform.right * gorkPushDistance;
+        gorkPushPositions[3] = transform.position - transform.right * gorkPushDistance;
+        gorkPushPositions[4] = transform.position + transform.forward * gorkPushDistance;
+        gorkPushPositions[5] = transform.position - transform.forward * gorkPushDistance;
+        
+        Vector3 closestPosition = gorkPushPositions[0];
+
+        foreach (var pos in gorkPushPositions)
+        {
+            if (Vector3.Distance(objPosition, pos) < Vector3.Distance(objPosition, closestPosition))
+            {
+                closestPosition = pos;
+            }
+        }
+
+        return closestPosition;
+    }
+    
 }
