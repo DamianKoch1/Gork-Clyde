4b705e69db952b977a5d6554f2244175f425d893
Damian <Damian@localhost>
2019-07-23 17:05:48 +0200

    n o r b

---
+++ Assets/Debug/test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class test : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private float hp;
+
+    [SerializeField]
+    private Rigidbody target;
+
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (!agent) return;
+        if (!target) return;
+        agent.SetDestination(target.position);
+        if (Vector3.Distance(transform.position, target.position) <= agent.stoppingDistance * 0.7f)
+        {
+            Attack(target.GetComponent<Player>());
+        }
+    }
+
+    private void Attack(Player target)
+    {
+        target.rb.AddForce(((target.rb.position - transform.position) * 3 + Vector3.up*6) * 20f, ForceMode.VelocityChange);
+    }
+}
