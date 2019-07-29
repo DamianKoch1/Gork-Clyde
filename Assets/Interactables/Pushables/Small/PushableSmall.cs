b388724bada28a9da06fce7f06451479d058a038
Damian <Damian@localhost>
2019-07-29 11:18:46 +0200

    clyde can now push, added Pushing class

--- Assets/Interactables/Pushables/Small/PushableSmall.cs
+++ Assets/Interactables/Pushables/Small/PushableSmall.cs
@@ -27,4 +27,32 @@ public class PushableSmall : Pushable
         }
     }
 
+    
+    /// <summary>
+    /// Enables clyde to push this object while in trigger
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.isTrigger) return;
+        var clyde = other.GetComponent<Clyde>();
+        if (!clyde) return;
+
+        clyde.pushing.pushedObj = gameObject;
+    }
+
+    /// <summary>
+    /// Prevents clyde from pushing this object after leaving it
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.isTrigger) return;
+        var clyde = other.GetComponent<Clyde>();
+        if (!clyde) return;
+        if (clyde.pushing.isPushing) return;
+        
+        clyde.pushing.pushedObj = null;
+        gameObject.layer = 0;
+    }
 }
--- Assets/Players/Gork.cs
+++ Assets/Players/Gork.cs
@@ -2,20 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(Throwing))]
+[RequireComponent(typeof(BoxCollider))]
 public class Gork : Player
 {
 	[HideInInspector]
-	public Throwing throwing;	
+	public Throwing throwing;
 
-	private FixedJoint fixedJoint;
+	
 
-	[HideInInspector]
-	public GameObject pushedObj;
 
-	[HideInInspector]
-	public bool isPushing;
 
 	public static string XAxis = "GorkHorizontal",
 	ZAxis = "GorkVertical",
@@ -29,32 +25,12 @@ public class Gork : Player
 		base.Start();
 		InitializeInputs(XAxis, ZAxis, JumpButton);
 		throwing = GetComponent<Throwing>();
-		throwing.anim = anim;
+		throwing.Initialize(anim);
 	}
 
-	protected override void Update()
-	{
-		base.Update();
-		if (isPushing)
-		{
-			CheckIfStillPushing();
-		}
-	}
+	
 
-	/// <summary>
-	/// Stops pushing if fixed joint broke or if falling
-	/// </summary>
-	private void CheckIfStillPushing()
-	{
-		if (!fixedJoint)
-		{
-			StopPushing();
-		}
-		else if (!state.wasGrounded)
-		{
-			StopPushing();
-		}
-	}
+	
 
 	protected override void CheckInput()
 	{
@@ -67,9 +43,9 @@ public class Gork : Player
 
 		if (Input.GetButtonUp(GorkInteract))
 		{
-			if (isPushing)
+			if (pushing.isPushing)
 			{
-				StopPushing();
+				pushing.StopPushing();
 			}
 		}
 	}
@@ -79,11 +55,11 @@ public class Gork : Player
 	/// </summary>
 	private void Interact()
 	{
-		if (pushedObj)
+		if (pushing.pushedObj)
 		{
 			if (!throwing.IsCarryingObject())
 			{
-				StartPushing();
+				pushing.StartPushing();
 			}
 		}
 		else
@@ -92,85 +68,11 @@ public class Gork : Player
 		}
 	}
 	
-	/// <summary>
-	/// Sets up pushing: adds fixed joint, changes movement, starts push animation
-	/// </summary>
-	private void StartPushing()
-	{
-		if (throwing.IsCarryingObject()) return;
-		if (isPushing) return;
-		if (fixedJoint) return;
-		if (!pushedObj) return;
-
-        anim.SetBool("push", true);
-		isPushing = true;
-		Rigidbody objectRb = pushedObj.GetComponent<Rigidbody>();
-		AlignToPushableSide();
-		ResetMotion();
-		setMotion = SetMotionPushing;
-		pushedObj.GetComponent<PushableBig>().isPushed = true;
-		AddFixedJoint(objectRb);
-	}
-
-	/// <summary>
-	/// Adds fixed joint connecting Gork with target, adapts breakForce to target mass
-	/// </summary>
-	/// <param name="target">RigidBody to connect joint with</param>
-	private void AddFixedJoint(Rigidbody target)
-	{
-		fixedJoint = gameObject.AddComponent<FixedJoint>();
-		fixedJoint.connectedBody = target;
-		fixedJoint.breakForce = rb.mass * 400;
-	}
 	
-	/// <summary>
-	/// Reverts movement, destroys fixedJoint if still there, stops push animation
-	/// </summary>
-	private void StopPushing()
-	{
-		if (fixedJoint)
-		{
-			Destroy(fixedJoint);
-		}
 
-		if (pushedObj)
-		{
-			pushedObj.GetComponent<PushableBig>().isPushed = false;
-			pushedObj = null;
-		}
+	
 
-		anim.SetBool("push", false);
-		isPushing = false;
-		ResetMotion();
-		setMotion = SetMotionDefault;
-	}
 
-	/// <summary>
-	/// Disables rotating to motion vector
-	/// </summary>
-	private void SetMotionPushing()
-	{
-		motion.x = Input.GetAxis(xAxis);
-		motion.z = Input.GetAxis(zAxis);
-
-		if (motion.magnitude > 1)
-		{
-			motion.Normalize();
-		}
-		
-		motion *= speed;
-		motion = CameraBehaviour.ApplyCameraRotation(motion);
-	}
-
-	private void AlignToPushableSide()
-	{
-		var pos = pushedObj.GetComponent<PushableBig>().GetClosestPushPosition(rb.position);
-		
-		pos.y = rb.position.y;
-		transform.position = pos;
-		
-		transform.LookAt(pushedObj.transform);
-	}
 
 	
 	
