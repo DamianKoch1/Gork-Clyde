e23ecff952ddc94db9e572a39571b375497321ce
Damian <Damian@localhost>
2019-07-29 12:37:35 +0200

    added missing script

---
+++ Assets/Players/Pushing.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pushing : MonoBehaviour
+{
+    private FixedJoint fixedJoint;
+
+    [HideInInspector]
+    public GameObject pushedObj;
+
+    [HideInInspector]
+    public bool isPushing;
+
+    private PlayerState state;
+    
+    private Animator anim;
+
+    private Rigidbody rb;
+
+    public delegate void OnPushStarted();
+    public OnPushStarted onPushStarted;
+    
+    public delegate void OnPushStopped();
+    public OnPushStopped onPushStopped;
+    
+    public void Initialize(Animator _anim, PlayerState _state, Rigidbody _rb)
+    {
+        anim = _anim;
+        state = _state;
+        rb = _rb;
+    }
+    
+    protected void Update()
+    {
+        if (isPushing)
+        {
+            CheckIfStillPushing();
+        }
+    }
+    
+    /// <summary>
+    /// Stops pushing if fixed joint broke or if falling
+    /// </summary>
+    private void CheckIfStillPushing()
+    {
+        if (!fixedJoint)
+        {
+            StopPushing();
+        }
+        else if (!state.wasGrounded)
+        {
+            StopPushing();
+        }
+    }
+    
+    /// <summary>
+    /// Sets up pushing: adds fixed joint, changes movement, starts push animation
+    /// </summary>
+    public void StartPushing()
+    {
+        if (GetComponent<Throwing>()?.IsCarryingObject() == true) return;
+        if (isPushing) return;
+        if (fixedJoint) return;
+        if (!pushedObj) return;
+
+        anim.SetBool("push", true);
+        isPushing = true;
+        Rigidbody objectRb = pushedObj.GetComponent<Rigidbody>();
+        AlignToPushableSide();
+        onPushStarted();
+        pushedObj.GetComponent<Pushable>().isPushed = true;
+        AddFixedJoint(objectRb);
+    }
+
+    /// <summary>
+    /// Adds fixed joint connecting Gork with target, adapts breakForce to target mass
+    /// </summary>
+    /// <param name="target">RigidBody to connect joint with</param>
+    private void AddFixedJoint(Rigidbody target)
+    {
+        fixedJoint = gameObject.AddComponent<FixedJoint>();
+        fixedJoint.connectedBody = target;
+        fixedJoint.breakForce = rb.mass * 400;
+    }
+
+    /// <summary>
+    /// Reverts movement, destroys fixedJoint if still there, stops push animation
+    /// </summary>
+    public void StopPushing()
+    {
+        if (fixedJoint)
+        {
+            Destroy(fixedJoint);
+        }
+
+        if (pushedObj)
+        {
+            pushedObj.GetComponent<Pushable>().isPushed = false;
+            pushedObj = null;
+        }
+
+        anim.SetBool("push", false);
+        isPushing = false;
+        onPushStopped();
+    }
+
+    private void AlignToPushableSide()
+    {
+        var pos = pushedObj.GetComponent<Pushable>().GetClosestPushPosition(rb.position);
+		
+        pos.y = rb.position.y;
+        transform.position = pos;
+
+        Vector3 lookAt = pushedObj.transform.position;
+        lookAt.y = pos.y;
+        transform.LookAt(lookAt);
+    }
+}
