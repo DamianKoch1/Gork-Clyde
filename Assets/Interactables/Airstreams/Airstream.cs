d2316dfedccc62748543631d184c77a32af9e0db
Damian <Damian@localhost>
2019-08-05 10:36:11 +0200

    airstream now toggles all child particlesystems regardless of count

--- Assets/Interactables/Airstreams/Airstream.cs
+++ Assets/Interactables/Airstreams/Airstream.cs
@@ -15,10 +15,6 @@ public class Airstream : MonoBehaviour, IActivatable
     private bool activeAtStart = true;
 
   
-    [SerializeField]
-    private ParticleSystem vfx1, vfx2;
-
-
     private void Start()
     {
         if (!activeAtStart)
@@ -109,14 +105,28 @@ public class Airstream : MonoBehaviour, IActivatable
         BoxCollider bc = GetComponent<BoxCollider>();
         if (bc.enabled)
         {
-            vfx1.Stop();
-            vfx2.Stop();
+            StopVfx();
         }
         else
         {
-            vfx1.Play();
-            vfx2.Play();
+            PlayVfx();
         }
         bc.enabled = !bc.enabled;
+    }
+
+    private void PlayVfx()
+    {
+        foreach (var vfx in  GetComponentsInChildren<ParticleSystem>())
+        {
+            vfx.Play();
+        }
+    }
+    
+    private void StopVfx()
+    {
+        foreach (var vfx in  GetComponentsInChildren<ParticleSystem>())
+        {
+            vfx.Stop();
+        }
     }
 }
