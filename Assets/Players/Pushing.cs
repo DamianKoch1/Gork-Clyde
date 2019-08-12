f277093f33adce7992eb71c4ea162b601e7b08b9
Damian <Damian@localhost>
2019-08-12 12:01:57 +0200

    implemented gork leg layer

--- Assets/Players/Pushing.cs
+++ Assets/Players/Pushing.cs
@@ -127,8 +127,14 @@ public class Pushing : MonoBehaviour
         transform.LookAt(lookAt);
     }
 
+    /// <summary>
+    /// Sets AngleToPushable parameter depending on motion angle to forward vector
+    /// </summary>
+    /// <param name="motion">Current movement motion</param>
     public void UpdateLegs(Vector3 motion)
     {
-        print(Vector3.Dot((pushedObj.transform.position - rb.position).normalized, (motion*transform.right).normalized));
+        var angle = Vector3.SignedAngle(transform.forward, motion.normalized, Vector3.up);
+        if (angle < 0) angle = 360 + angle;
+        anim.SetFloat("AngleToPushable", angle);
     }
 }
