f66256c506ef99ba003106a5f59cc24a117baa5a
Damian <Damian@localhost>
2019-08-23 12:42:22 +0200

    -added player shadow -airstream vfx warms up on activation -collision vfx now word space simulated

---
+++ Assets/Players/PlayerShadow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attach to any object to make it cast a shadow on the ground
+/// </summary>
+public class PlayerShadow : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.Raycast(transform.parent.position, -Vector3.up, out var raycastHit, Mathf.Infinity, Physics.AllLayers,
+            QueryTriggerInteraction.Ignore);
+        transform.position = raycastHit.point + Vector3.up*0.1f;
+
+    }
+}
